53838d56a34060f84080cb5bba661392acca31a1
Elizaveta Markova <Lizok.pattrk@gmail.com>
2024-02-27 02:06:24 +0300

    add new array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
 ﻿Console.Clear();
+
+// Ввод количества строк
+Console.WriteLine("Введите количество строк:");
+int n = int.Parse(Console.ReadLine());
+
+string[] array = new string[n];
+
+// Ввод строк
+for (int i = 0; i < n; i++)
+{
+  Console.WriteLine("Введите строку {0}:", i + 1);
+  array[i] = Console.ReadLine();
+}
+
