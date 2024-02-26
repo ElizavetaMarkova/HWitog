7f8bc9a93ce62eb6ee4cb411f870e5b48a9dc6f1
Elizaveta Markova <Lizok.pattrk@gmail.com>
2024-02-27 02:07:24 +0300

    add short arr

--- Program.cs
+++ Program.cs
@@ -13,3 +13,17 @@ for (int i = 0; i < n; i++)
   array[i] = Console.ReadLine();
 }
 
+string[] new_array = new string[n];
+int j = 0;
+
+for(int i = 0; i < n; i++)
+{
+    if(array[i].Length <= 3)
+    {
+      new_array[j] = array[i];
+      j++;
+    }
+}
+
+Console.WriteLine("Вывод нового массива: ");
+foreach(string e in new_array) Console.WriteLine(e);
